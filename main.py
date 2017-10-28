d4e9de9dda96101558b9f28f2af41453f2ff0afd
luismora2297 <luismora1997@gmail.com>
2017-10-27 20:58:02 -0500

    Uploading first comments and the two major folders

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+	Luis Mora @ fobos
+	Developed by BlackBohr
+	Apache License 2.0
+	IronSnake is a numerical methods calculator with various functions able to do most of
+	the many methods explained in the book 'Applied Numerical Methods in C by Shoichiro Nakamura'
+"""
+
+#Begin Imports
