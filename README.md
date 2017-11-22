4a216e16f365ea4060ff78ab80ed87126a83db9e
Luis Daniel Mora Delgado <luismora1997@gmail.com>
2017-11-22 11:30:29 -0500

    Uploading Only Beta

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ At the same time, the development has been divided in two major folders, which, 
 If the user install the program as a GUI, then, it has to be stablished that mainly, the application is only working on Linux Distros, if you use Mac OS, BSD, Windows or any other Operative System, at the time, it has not support for them, just only linux, if you wish to contribute to port the program to other OS's, feel free to fork the repo or contribute to the code, but first contact me personally to add the permissions to allow modification in the repo, and to put your name in the credits, *"the more heads and hands that contribute to the code, the more interesting and powerfull it gets"*.
 
 This application is developed in Python 3.X.X, i haven't try on Python 2.X.X so, maybe most part of the application may not work, please, leave behind older versions of python and work with Python 3!
+
+#### Credits
+
+BlacBohr. *"When the Owl meets the Python..."*
--- main.py
+++ main.py
@@ -5,5 +5,100 @@
 	IronSnake is a numerical methods calculator with various functions able to do most of
 	the many methods explained in the book 'Applied Numerical Methods in C by Shoichiro Nakamura'
 """
+# Begin Imports
+import sys
+import numpy
 
-#Begin Imports
+# Begin App
+IronCalcLagrangeInterpolate = True
+print("Bienvenido al programa de Interpolación de Lagrange")
+while IronCalcLagrangeInterpolate:
+    validate = True
+    while validate:
+        # Petición de cantidad de números a interpolar
+        xyz = input("Introduzca la cantidad de números a interpolar: ")
+        try:
+            i = int(xyz)
+            # Confirmando nuestra cantidad de números a interpolar
+            # print(i)
+            # Condición que tiene nuestro metodo
+            if i > 4:
+                print("Advertencia, utilizar el metodo de interpolación de Lagrange para más de 4 datos, esta data puede quedar obsoleta o incorrecta en su precisión.")
+            break
+        except ValueError:
+            print("Número no valido... ")
+
+    while validate:
+        # Valor que le pedimos al usuario para hacer el calculo de interpolación con Lagrange
+        xenter = input("Introduzca valor de interpolación X: ")
+        try:
+            xvalue = float(xenter)
+            # Confirmando nuestra cantidad de números a interpolar
+            # print(xvalue)
+            break
+        except ValueError:
+            print("Número no valido... ")
+
+    # Inicializando arrays en Python 3
+    lg = []
+    fx = []
+
+    # Pedimos los números independientes
+    x = 1
+    for x in range(i):
+        while x < i:
+            xyu = input("Introduzca el número independiente: ")
+            try:
+                x = float(xyu)
+                lg.append(x)
+                x += 1
+            except ValueError:
+                print("Número incorrecto, introduzca denuevo el número")
+    # Impresión para verificación
+    # print(len(lg))
+    # print(*lg)
+
+    # Pedimos los números dependientes
+    y = 1
+    for y in range(i):
+        while y < i:
+            try:
+                y = float(input("Introduzca el número dependiente: "))
+                fx.append(y)
+                y += 1
+            except ValueError:
+                print("Número incorrecto, introduzca denuevo número")
+    # Impresión para verificación
+    # print(len(fx))
+    # print(*fx)
+    # Imprimir tabla
+    print("i    x   f(x)")
+    z = 0
+    for number1, number2 in zip(lg, fx):
+        rowprint = str(z) + "   "+str(number1)+"  "+str(number2)
+        z += 1
+        print ( rowprint )
+
+    # Aplicando la formula de Lagrange
+    U = fx
+    T = lg
+    p = 0
+    for j in range(len(lg)):
+        # print(U[j])
+        pt = U[j]
+        for k in range(len(lg)):
+            if k == j:
+                continue
+            fac = T[j] - T[k]
+            # print(fac)
+            polynomium = numpy.poly1d([1.0, -T[k]])
+            # print(polynomium)
+            pt *= polynomium / fac
+            # print(pt)
+        p += pt
+        # print(p)
+    print(p)
+
+    IronCalcLagrangeInterpolate = False
+# Sale del programa
+sys.exit(0)
